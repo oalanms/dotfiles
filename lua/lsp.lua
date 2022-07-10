8726e9812c6a7f8a592df2814ae695d3ee2f98c5
Alan M Silva <alanfortlink@gmail.com>
2022-07-10 18:40:42 +0100

    Update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -64,7 +64,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 	vim.keymap.set('n', '<F4>', ':FlutterDevices<CR>')
 	vim.keymap.set('n', '<F5>', ':FlutterRun<CR>')
-	vim.keymap.set('n', '<F6>', ':FlutterVisualDebug<CR>')
+	vim.keymap.set('n', '<F6>', ':FlutterVSplit<CR>')
 	vim.keymap.set('n', '<F7>', ':FlutterHotRestart<CR>')
 	vim.keymap.set('n', '<F8>', ':FlutterQuit<CR>')
 	end
