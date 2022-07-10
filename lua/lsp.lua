e4fd7a18d6bcc905e61594a8f84c119846d8a4ed
Alan M Silva <alanfortlink@gmail.com>
2022-07-10 20:04:14 +0100

    Update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -46,6 +46,15 @@ vim.api.nvim_create_autocmd("FileType", {
 
 local cmp = require('cmp')
 cmp.setup({
+  sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
 })
 
 -- Old coc.nvim stuff
