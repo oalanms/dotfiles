d92fba9016f4d24e0d22db6ce87797d78629297a
Alan M Silva <alanfortlink@gmail.com>
2022-07-11 22:36:24 +0100

    Update

--- lua/style.lua
+++ lua/style.lua
@@ -16,7 +16,7 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = {"cpp", "c", "h", "hpp"}, 
+	pattern = {"cpp", "c", "h", "hpp"},
 	callback = function()
 	vim.o.shiftwidth = 4
 	vim.o.tabstop = 4
