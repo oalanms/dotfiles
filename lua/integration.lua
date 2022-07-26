c2340bde398e5a176ab94f39c7dd74ea488caa77
Alan Martins Silva <asilva173@bloomberg.net>
2022-07-26 12:36:39 -0400

    Update

--- lua/integration.lua
+++ lua/integration.lua
@@ -1,4 +1,5 @@
 local jobs = {}
+local cur_qf = {}
 
 vim.keymap.set('n', '<localleader>jb', ':lua run_make_job("build")<CR>', no_remap_silent)
 vim.keymap.set('n', '<localleader>jr', ':lua run_make_job("run")<CR>', no_remap_silent)
@@ -7,12 +8,14 @@ vim.keymap.set('n', '<localleader>jq', ':lua stop_jobs()<CR>', no_remap_silent)
 
 function append_to_quickfix(data)
   for _, line in ipairs(data) do
-    vim.cmd("cadde " .. "\"" .. line .. "\"")
+    cur_qf[#cur_qf+1] = line
   end
+  vim.diagnostic.fromqflist(cur_qf)
 end
 
 function run_make_job(target)
   vim.cmd("cexpr []")
+  cur_qf = {}
   append_to_quickfix({"make " .. target})
   job = vim.fn.jobstart({"make", target}, {
     stdout_buffered = true,
@@ -20,7 +23,7 @@ function run_make_job(target)
       append_to_quickfix({"Finished with rc=" .. tostring(rc)})
     end,
     on_stdout = function(_, data)
-      append_to_quickfix(data)
+      -- append_to_quickfix(data)
     end,
     on_stderr = function(_, data)
       append_to_quickfix(data)
