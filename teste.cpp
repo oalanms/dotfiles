b39b6f0d63dc7bc6efc7aa9c72e846ac73ecaccc
Alan M Silva <alanfortlink@gmail.com>
2022-08-05 20:43:43 +0100

    Update

--- teste.cpp
+++ teste.cpp
@@ -1,3 +1,15 @@
+class Calculator {
+    public:
+    int sum(int a, int b) { return a + b; }
+    int sub(int a, int b) { return a - b; }
+};
+
 int main(){
+
+    Calculator calc;
+    int a = 1, b = 2;
+    int result = calc.sum(a, b);
+    int result2 = calc.sub("a", "b");
+
     return 0;
 }
