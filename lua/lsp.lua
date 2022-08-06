bb1b6f580202d3727462f46e71c163f2a8a85fd3
Alan M Silva <alanfortlink@gmail.com>
2022-08-06 18:40:55 +0100

    Update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -26,19 +26,20 @@ end
 
 local cmp = require('cmp')
 
+-- TODO: Copied from README.md of nvim-cmp. What can I remove?
 cmp.setup({
 snippet = {
   -- REQUIRED - you must specify a snippet engine
   expand = function(args)
-    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
   end,
 },
 window = {
-  -- completion = cmp.config.window.bordered(),
-  -- documentation = cmp.config.window.bordered(),
+  completion = cmp.config.window.bordered(),
+  documentation = cmp.config.window.bordered(),
 },
 mapping = cmp.mapping.preset.insert({
   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -49,39 +50,12 @@ mapping = cmp.mapping.preset.insert({
 }),
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
-  -- { name = 'vsnip' }, -- For vsnip users.
   { name = 'luasnip' }, -- For luasnip users.
+  -- { name = 'vsnip' }, -- For vsnip users.
   -- { name = 'ultisnips' }, -- For ultisnips users.
   -- { name = 'snippy' }, -- For snippy users.
 }, {
   { name = 'buffer' },
-})
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-  { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-}, {
-  { name = 'buffer' },
-})
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-mapping = cmp.mapping.preset.cmdline(),
-sources = {
-  { name = 'buffer' }
-}
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-mapping = cmp.mapping.preset.cmdline(),
-sources = cmp.config.sources({
-  { name = 'path' }
-}, {
-  { name = 'cmdline' }
 })
 })
 
