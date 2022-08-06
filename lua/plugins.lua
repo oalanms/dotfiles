362b302dd137242d9099489f10135d07f6e40c51
Alan M Silva <alanfortlink@gmail.com>
2022-08-06 22:29:24 +0100

    Update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,38 +40,38 @@ require('packer').startup(function()
     use 'airblade/vim-gitgutter'
     use 'kshenoy/vim-signature'
     use 'tomasiser/vim-code-dark'
-    use {
-        'nvim-lualine/lualine.nvim',
-    }
+    -- use {
+    --     'nvim-lualine/lualine.nvim',
+    -- }
 end
 )
 
-require('lualine').setup({
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {},
-        always_divide_middle = true,
-        globalstatus = false,
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'filesize', 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'filesize', 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    tabline = {},
-    extensions = {}
-})
+-- require('lualine').setup({
+--     options = {
+--         icons_enabled = true,
+--         theme = 'auto',
+--         component_separators = { left = '', right = '' },
+--         section_separators = { left = '', right = '' },
+--         disabled_filetypes = {},
+--         always_divide_middle = true,
+--         globalstatus = false,
+--     },
+--     sections = {
+--         lualine_a = { 'mode' },
+--         lualine_b = { 'branch', 'diff', 'diagnostics' },
+--         lualine_c = { 'filename' },
+--         lualine_x = { 'filesize', 'encoding', 'filetype' },
+--         lualine_y = { 'progress' },
+--         lualine_z = { 'location' }
+--     },
+--     inactive_sections = {
+--         lualine_a = { 'mode' },
+--         lualine_b = { 'branch', 'diff', 'diagnostics' },
+--         lualine_c = { 'filename' },
+--         lualine_x = { 'filesize', 'encoding', 'filetype' },
+--         lualine_y = { 'progress' },
+--         lualine_z = { 'location' }
+--     },
+--     tabline = {},
+--     extensions = {}
+-- })
